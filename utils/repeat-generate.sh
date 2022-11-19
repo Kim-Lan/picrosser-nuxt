223e01e8cb2bf5ba6d20524c9591cf05e21527fd
Kim-Lan <kimlan.vo@proton.me>
2022-11-19 10:53:57 -0800

    generating puzzles and convert to json

---
+++ utils/repeat-generate.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+_stopnow() {
+  test -f stopnow && echo "Stopping!" && rm stopnow && exit 0 || return 0
+}
+
+while true
+do
+    _stopnow
+    # Below here, you put in your command you want to run:
+
+    ts-node generate.ts
+done
