93056838511cb9aa638d453ba5ee194688df4b80
Kim-Lan <me@kim-lan.com>
2024-03-30 12:32:32 -0700

    size selector

--- pages/play/[height]x[width].vue
+++ pages/play/[height]x[width].vue
@@ -2,14 +2,43 @@
 const route = useRoute();
 const height = route.params.height;
 const width = route.params.width;
-const sizes = ['5x5', '10x10', '15x15', '20x20', '25x25'];
-
+// const sizes = ['5x5', '10x10', '15x15', '20x20', '25x25'];
+const sizes = [
+  {
+    value: 5,
+    label: '5x5'
+  },
+  {
+    value: 10,
+    label: '10x10'
+  },
+  {
+    value: 15,
+    label: '15x15'
+  },
+  {
+    value: 20,
+    label: '20x20'
+  },
+  {
+    value: 25,
+    label: '25x25'
+  }
+];
+const selectedSize = ref({
+  value: height,
+  label: `${height}x${height}`
+});
 const stopwatch = useStopwatch();
+stopwatch.reset();
 
 const puzzleComponent = ref(null);
 const statusMessage = ref('Keep going!');
 
 async function newPuzzleHandler() {
+  if (selectedSize.value != height) {
+    navigateTo(`/play/${selectedSize.value}x${selectedSize.value}`);
+  }
   const newPuzzleID = await puzzleComponent.value.loadPuzzle();
   puzzleComponent.value.resetCells();
   console.log("new puzzle id " + newPuzzleID);
@@ -27,17 +56,28 @@ function solved() {
 <template>
   <div class="flex flex-col items-center gap-10 my-10 w-full">
     <div class="flex flex-col items-center">
+      <!-- <select>
+        <option value="5">5x5</option>
+        <option value="10">10x10</option>
+        <option value="15">15x15</option>
+        <option value="20">20x20</option>
+        <option value="25">25x25</option>
+      </select> -->
       <v-select
-        class="w-60"
+        v-model="selectedSize"
         :items="sizes"
         density="compact"
         label="Select Size"
+        item-title="label"
+        item-value="value"
+        class="w-60"
       ></v-select>
       <div class="flex flex-row items-center gap-5">
 
         <v-btn @click="newPuzzleHandler">Start New</v-btn>
         <v-btn>Check</v-btn>
         <v-btn>Restart</v-btn>
+        <v-btn></v-btn>
       </div>
     </div>
     <div class="text-5xl font-bold">
