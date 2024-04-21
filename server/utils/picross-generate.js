5f44287259b2b573adbfa7afe1581fbaf868aa20
Kim-Lan <me@kim-lan.com>
2024-04-21 15:33:06 -0700

    fix server directory path

--- server/utils/picross-generate.js
+++ server/utils/picross-generate.js
@@ -106,8 +106,8 @@ export function bitGenerate(size) {
 export function checkUnique(grid) {
   const __dirname = path.resolve(path.dirname(''));
   const data = gridToNon(grid);
-  const testPath = path.join(__dirname, 'server', 'generate', 'test.non');
-  const naughtyPath = path.join(__dirname, 'server', 'modules', 'naughty');
+  const testPath = path.join(__dirname, 'picrosser.com', 'server', 'generate', 'test.non');
+  const naughtyPath = path.join(__dirname, 'picrosser.com', 'server', 'modules', 'naughty');
   fs.writeFileSync(testPath, data);
   const check1 = (execSync(naughtyPath + ' -u < ' + testPath)).toString();
   //fs.unlinkSync(testPath);
