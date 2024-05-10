4a661918891155284a03f0c2a9c09f695c611a8a
Kim-Lan <me@kim-lan.com>
2024-05-10 13:20:41 -0700

    Start register api route

--- server/api/auth/register.post.ts
+++ server/api/auth/register.post.ts
@@ -1,3 +1,17 @@
-export default defineEventHandler((event) => {
-  
+import { User } from '~/server/models/User';
+
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event);
+
+  if (!body.username || !body.email || !body.password) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'Bad Request',
+      message: 'Missing required fields',
+    })
+  }
+
+  const user = await User.create(body);
+
+  return user;
 })
