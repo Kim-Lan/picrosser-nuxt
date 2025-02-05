e035897a6b33760257558acaa08dcf081ae862e5
Kim-Lan <me@kim-lan.com>
2025-02-04 16:29:22 -0800

    Get existing puzzles from database for sizes greater than 5

--- server/api/puzzle/getNewPuzzle.get.ts
+++ server/api/puzzle/getNewPuzzle.get.ts
@@ -14,66 +14,52 @@ export default defineEventHandler(async (event) => {
     })
   }
 
-  if (height == 25 && width == 25) {
-    const puzzle = await Puzzle.aggregate([
-      {
-        $match: { height: 25, width: 25 }
-      },
-      {
-        $sample: { size: 1 }
-      }
-    ]).exec();
-    if (!puzzle[0]) {
-      throw createError({
-        statusCode: 500,
-        statusMessage: 'Failed to find puzzle'
-      })
+  if (height == 5) {
+    solution = bitGenerate(height);
+
+    const { rowKeys, colKeys } = getKeys(solution);
+    const goal = getGoalString(solution);
+
+    const existingPuzzle = await Puzzle.findOne({ goal });
+    let puzzleId = '';
+    if (existingPuzzle) {
+      puzzleId = existingPuzzle._id;
+    } else {
+      const newPuzzle = await Puzzle.create({
+        width,
+        height,
+        rowKeys,
+        colKeys,
+        goal
+      });
+      puzzleId = newPuzzle._id;
     }
     return {
-      puzzleId: puzzle[0]._id,
-      rowKeys: puzzle[0].rowKeys,
-      colKeys: puzzle[0].colKeys,
-      solution: puzzle[0].goal,
-    }
-  }
-
-  // console.log("loading puzzle " + height + "x" + width);
-  let solution;
-  if (height == 5 && width == 5) {
-    solution = bitGenerate(width);
-  // } else if (height == 25 && width == 25) {
-  //   solution = generate25x25();
-  } else {
-    solution = generate(height, width);
-  }
-  const { rowKeys, colKeys } = getKeys(solution);
-  const goal = getGoalString(solution);
-
-  const existingPuzzle = await Puzzle.findOne({ goal });
-  let puzzleId = '';
-  if (existingPuzzle) {
-    puzzleId = existingPuzzle._id;
-  } else {
-    const newPuzzle = await Puzzle.create({
-      width,
-      height,
+      puzzleId,
       rowKeys,
       colKeys,
-      goal
-    });
-    puzzleId = newPuzzle._id;
-    // puzzleId = new mongoose.Types.ObjectId();
+      solution
+    };
   }
 
-  // console.log("returning puzzle");
-  // console.log("id " + puzzleId);
-  // printArray(rowKeys);
-  // printArray(colKeys);
-  // printGrid(solution);
+  const puzzle = await Puzzle.aggregate([
+    {
+      $match: { height: parseInt(height), width: parseInt(width) }
+    },
+    {
+      $sample: { size: 1 }
+    }
+  ]).exec();
+  if (!puzzle[0]) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'Failed to find puzzle'
+    })
+  }
   return {
-    puzzleId,
-    rowKeys,
-    colKeys,
-    solution
-  };
+    puzzleId: puzzle[0]._id,
+    rowKeys: puzzle[0].rowKeys,
+    colKeys: puzzle[0].colKeys,
+    solution: puzzle[0].goal,
+  }
 })
